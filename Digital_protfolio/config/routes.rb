9a47b6cc493e86880738d96b07439ac55e86ce49
RaulEV1411 <136620455+RaulEV1411@users.noreply.github.com>
2023-10-20 17:41:25 -0600

    AñadidoGemaDivese

--- Digital_protfolio/config/routes.rb
+++ Digital_protfolio/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   
   resources :skills
   resources :social_links
@@ -8,13 +9,14 @@ Rails.application.routes.draw do
   resources :projects
   resources :users
 
+  devise_scope :user do
+    get "/about/sign_out", as: "sign_out", to: "devise/sessions#destroy"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "about#index"
-  
-  get "/", to: "about#about"
-  get "/about", to: "about#about"
 
 end
+    
--- Digital_protfolio/db/schema.rb
+++ Digital_protfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_211201) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_20_211407) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -106,7 +106,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_211201) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "social_links_id", null: false
+    t.integer "social_links_id"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["social_links_id"], name: "index_users_on_social_links_id"
   end
 
